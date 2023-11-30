f641ad2a91aac4a8903749841255bf0bba78c7e8
DianaSemenova <menestrelia199702@gmail.com>
2023-11-30 23:41:07 +0400

    feat: added component DirectionCoursesInfo

--- src/pages/coursesInfo/CoursesInfo.jsx
+++ src/pages/coursesInfo/CoursesInfo.jsx
@@ -1,12 +1,14 @@
 import s from "./CoursesInfo.module.css";
 import HeaderCoursesInfo from "../../components/coursesInfoHeader/HeaderCourses";
 import FittingCoursesInfo from "../../components/coursesInfoFitting/FittingCourses";
+import DirectionCoursesInfo from "../../components/coursesInfoDirection/DirectionCourses";
 
 export default function CoursesInfoPage() {
     return (
         <div className={s.coursesDiv + " " + s.center}>
             <HeaderCoursesInfo />
             <FittingCoursesInfo />
+            <DirectionCoursesInfo />
         </div>
     );
 }
