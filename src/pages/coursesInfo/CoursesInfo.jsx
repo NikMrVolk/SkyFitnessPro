1650d8027ff09c63707e3dd73ce726fd1adb3b29
DianaSemenova <menestrelia199702@gmail.com>
2023-11-30 22:52:23 +0400

    feat: added component FittingCourses

--- src/pages/coursesInfo/CoursesInfo.jsx
+++ src/pages/coursesInfo/CoursesInfo.jsx
@@ -1,10 +1,12 @@
 import s from "./CoursesInfo.module.css";
-import HeaderCoursesInfo from "../../components/headerCoursesInfo/HeaderCourses";
+import HeaderCoursesInfo from "../../components/coursesInfoHeader/HeaderCourses";
+import FittingCoursesInfo from "../../components/coursesInfoFitting/FittingCourses";
 
 export default function CoursesInfoPage() {
     return (
         <div className={s.coursesDiv + " " + s.center}>
             <HeaderCoursesInfo />
+            <FittingCoursesInfo />
         </div>
     );
 }
