449bb3521f4cd7c6dedfdd8d3417f95e2468768b
Ulyana <ushinkevich@mail.ru>
2023-12-04 11:51:59 +0400

    chore: new folder mock data

--- src/mock/CoursesData.jsx
+++ src/mock/CoursesData.jsx
@@ -2,19 +2,19 @@ export const UserCourses = [
     {
         id: 1,
         name: 'Йога',
-        img: '../img/card1.svg',
+        img: '../img/courses/card1.svg',
         alt: 'yoga',
     },
     {
         id: 2,
         name: 'Стретчинг',
-        img: '../img/card2.svg',
+        img: '../img/courses/card2.svg',
         alt: 'stretching',
     },
     {
         id: 3,
         name: 'Бодифлекс',
-        img: '../img/card5.svg',
+        img: '../img/courses/card5.svg',
         alt: 'bodyflex',
     },
 ]
@@ -23,31 +23,31 @@ export const AllCourses = [
     {
         id: 1,
         name: 'Йога',
-        img: '../img/card1.svg',
+        img: '../img/courses/card1.svg',
         alt: 'yoga',
     },
     {
         id: 2,
         name: 'Стретчинг',
-        img: '../img/card2.svg',
+        img: '../img/courses/card2.svg',
         alt: 'stretching',
     },
     {
         id: 4,
         name: 'Танцевальный фитнес',
-        img: '../img/card3.svg',
+        img: '../img/courses/card3.svg',
         alt: 'stretching',
     },
     {
         id: 5,
         name: 'Степ-аэробика',
-        img: '../img/card4.svg',
+        img: '../img/courses/card4.svg',
         alt: 'yoga',
     },
     {
         id: 5,
         name: 'Бодифлекс',
-        img: '../img/card5.svg',
+        img: '../img/courses/card5.svg',
         alt: 'bodyflex',
     },
 ]
