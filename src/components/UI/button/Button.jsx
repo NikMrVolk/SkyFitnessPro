f51746ec4b1e337604fca411e83ff6230fb76ddf
Ilmira260696 <130978593+Ilmira260696@users.noreply.github.com>
2023-12-05 10:11:26 +0400

    стр авторизации

--- src/components/UI/button/Button.jsx
+++ src/components/UI/button/Button.jsx
@@ -1,21 +1,5 @@
-import { useLocation } from 'react-router-dom'
-import { MAIN_ROUTE } from '../../../utils/constants'
 import s from '../../../styles/components/UI/button/Button.module.css'
 
-// const Button = ({ children, ...props }) => {
-//     const { pathname } = useLocation()
-
-//     const styles = (pathname  ? s.loginBtn : s.btn) 
-    
-    
-//     return (
-//         <button {...props} className={styles}>
-//             {children}
-//         </button>
-//     )
-// }
-
-// export default Button
 function Button({ text, color, onClick }) {
     return (
       <button className={`${s.button} ${s[color]}`} onClick={onClick}>
