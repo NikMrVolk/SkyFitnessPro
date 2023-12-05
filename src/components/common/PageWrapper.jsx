ad9fbb0979e5ae012c2e00652159b4ace12cd443
Ilmira260696 <130978593+Ilmira260696@users.noreply.github.com>
2023-12-05 10:22:16 +0400

    подправила стр рег

--- src/components/common/PageWrapper.jsx
+++ src/components/common/PageWrapper.jsx
@@ -1,13 +1,14 @@
 import { useLocation } from 'react-router-dom'
 import { MAIN_ROUTE } from '../../utils/constants'
 import { LOGIN_ROUTE } from '../../utils/constants'
+import { REGISTRATION_ROUTE } from '../../utils/constants'
 import s from '../../styles/components/common/PageWrapper.module.css'
 
 function PageWrapper({ children }) {
     const { pathname } = useLocation()
 
     const styles = [s.wrapper, 
-        pathname === MAIN_ROUTE || LOGIN_ROUTE ? s.main : ''].join(' ')
+        pathname === MAIN_ROUTE || LOGIN_ROUTE || REGISTRATION_ROUTE ? s.main : ''].join(' ')
         
 
     return <div className={styles}>{children}</div>
