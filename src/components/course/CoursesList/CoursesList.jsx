74df5c676a752c5f7a451493842d6578d0c56280
Ulyana <ushinkevich@mail.ru>
2023-12-05 08:36:51 +0400

    refactor: courses list

--- src/components/course/CoursesList/CoursesList.jsx
+++ src/components/course/CoursesList/CoursesList.jsx
@@ -3,28 +3,15 @@ import s from './CoursesList.module.css'
 function CoursesList({ courses, mainPage }) {
     return (
         <div className={s.courses}>
-            {mainPage
-                ? courses.map((el) => (
-                      <div className={s.course} key={el.id}>
-                          <span className={s.courseName}>{el.name}</span>
-                          <img
-                              className={s.courseImg}
-                              src={el.img}
-                              alt={el.alt}
-                          />
-                      </div>
-                  ))
-                : courses.map((el) => (
-                      <div className={s.course} key={el.id}>
-                          <span className={s.courseName}>{el.name}</span>
-                          <img
-                              className={s.courseImg}
-                              src={el.img}
-                              alt={el.alt}
-                          />
-                          <button className={s.courseBtn}>Перейти →</button>
-                      </div>
-                  ))}
+            {courses.map((el) => (
+                <div className={s.course} key={el.id}>
+                    <span className={s.courseName}>{el.name}</span>
+                    <img className={s.courseImg} src={el.img} alt={el.alt} />
+                    {mainPage ? null : (
+                        <button className={s.courseBtn}>Перейти →</button>
+                    )}
+                </div>
+            ))}
         </div>
     )
 }
