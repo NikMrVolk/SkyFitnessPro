e47cda29bd19a524d4b036052cd33cc9bd5d6725
DianaSemenova <menestrelia199702@gmail.com>
2023-12-13 13:13:40 +0400

    feat: added RTK Query: getCourses, getWorkouts

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,9 @@ import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 import AppRoutes from './components/routes/AppRoutes'
 import store from './store/store'
-import { useState } from 'react'
 
 function App() {
-    // const [user, setUser] = useState(localStorage.getItem('auth') || null)
     const user = JSON.parse(localStorage.getItem('auth')) || null
-    // const user = true
-    console.log('user', user)
 
     return (
         <Provider store={store}>
--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit'
 import profileSlice from './slices/profileSlice'
 import authSlice from './slices/authSlice'
+import { coursesQuery } from '../services/courses'
 
 const store = configureStore({
     reducer: {
         profile: profileSlice,
         auth: authSlice,
+        [coursesQuery.reducerPath]: coursesQuery.reducer,
     },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(coursesQuery.middleware),
 })
 
 export default store
