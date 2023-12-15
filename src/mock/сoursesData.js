761c5521ee04d044a7efca8855265256a655e65e
Ulyana <ushinkevich@mail.ru>
2023-12-15 13:47:47 +0400

    chore: img course info header

--- src/mock/сoursesData.js
+++ src/mock/сoursesData.js
@@ -24,30 +24,35 @@ export const allCourses = [
         id: 1,
         name: 'Йога',
         img: '../img/courses/card1.svg',
+        imgHeader: '../img/coursesHeader/card1.svg',
         alt: 'yoga',
     },
     {
         id: 2,
         name: 'Стретчинг',
         img: '../img/courses/card2.svg',
+        imgHeader: '../img/coursesHeader/card2.svg',
         alt: 'stretching',
     },
     {
         id: 4,
         name: 'Танцевальный фитнес',
         img: '../img/courses/card3.svg',
+        imgHeader: '../img/coursesHeader/card3.svg',
         alt: 'stretching',
     },
     {
         id: 5,
         name: 'Степ-аэробика',
         img: '../img/courses/card4.svg',
+        imgHeader: '../img/coursesHeader/card4.svg',
         alt: 'yoga',
     },
     {
         id: 6,
         name: 'Бодифлекс',
         img: '../img/courses/card5.svg',
+        imgHeader: '../img/coursesHeader/card5.svg',
         alt: 'bodyflex',
     },
 ]
@@ -82,5 +87,5 @@ export const exercises = [
         text: 'Гибкость спины',
         day: 'Йога на каждый день / 5 день',
         done: false,
-    }
+    },
 ]
