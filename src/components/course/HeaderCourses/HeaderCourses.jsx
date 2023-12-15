f4015af1b72bf403939d69e3cc7ba16bbff2d204
DianaSemenova <menestrelia199702@gmail.com>
2023-12-15 20:14:12 +0400

    chore: changed the array allCourses to imgCourses in HeaderCoursesInfo

--- src/components/course/HeaderCourses/HeaderCourses.jsx
+++ src/components/course/HeaderCourses/HeaderCourses.jsx
@@ -1,20 +1,19 @@
 import { allCourses } from '../../../mock/сoursesData'
 import s from './HeaderCourses.module.css'
+import { imgCourses } from '../../../mock/сoursesData'
 
 export default function HeaderCoursesInfo({ course }) {
+    console.log(course)
     return (
-
         <header className={s.header}>
             <div className={s.header}>
                 <h1 className={s.title}>{course.nameRU}</h1>
-             <img
-               className={s.img}
-                src={allCourses[0].imgHeader}
-                alt={allCourses[0].alt}
-            />
-             
+                <img
+                    className={s.img}
+                    src={imgCourses[course.order - 1].imgHeader}
+                    alt={imgCourses[course.order - 1].alt}
+                />
             </div>
         </header>
-
     )
 }
