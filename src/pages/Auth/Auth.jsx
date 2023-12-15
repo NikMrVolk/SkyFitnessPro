faef1449f29fc5fd1df68c979f562e89faad920e
NickZavadski <nick.mr.volk@gmail.com>
2023-12-15 06:05:49 +0300

    feat: add validation to auth inputs, config tostify

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -1,17 +1,23 @@
-import { useLocation, useNavigate, Link } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 import {
     getAuth,
     signInWithEmailAndPassword,
     createUserWithEmailAndPassword,
 } from 'firebase/auth'
+import { toast } from 'react-toastify'
 import Button from '../../components/UI/button/Button'
-import { LOGIN_ROUTE, PROFILE_ROUTE, MAIN_ROUTE } from '../../utils/constants'
+import { LOGIN_ROUTE, PROFILE_ROUTE } from '../../utils/constants'
 import { REGISTRATION_ROUTE } from '../../utils/constants'
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import Input from '../../components/UI/input/Input'
 import s from './Auth.module.css'
 import { useDispatch } from 'react-redux'
 import { setAuth } from '../../store/slices/authSlice'
+import {
+    samePasswords,
+    validateEmail,
+    validatePassword,
+} from '../../utils/authValidate'
 
 export function Auth() {
     const dispatch = useDispatch()
@@ -20,72 +26,35 @@ export function Auth() {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [repeatPassword, setRepeatPassword] = useState('')
-    const [errorState, setErrorState] = useState('')
+    const [validationError, setValidationError] = useState('')
+    const isLogin = pathname === LOGIN_ROUTE
 
-    useEffect(() => {
-        console.log('errorState2:', errorState)
-    }, [errorState])
-    
-    const handleLogin = async () => {
-        const auth = getAuth()
-        if (!email.trim()) {
-            return setError('Введите email')
-        }
-        if (!password.trim()) {
-            return setError('Введите пароль')
-        }
-        if (password.length < 6) {
-            return setError('Пароль должен содержать не менее 6 символов')
-        }
+    const auth = async () => {
+        const emailAfterTrim = email.trim()
 
-        signInWithEmailAndPassword(auth, email, password)
-            .then(({ user }) => {
-                dispatch(
-                    setAuth({
-                        accessToken: user.accessToken,
-                        email: user.email,
-                        uid: user.uid,
-                        refreshToken: user.stsTokenManager.refreshToken,
-                    }),
+        if (
+            validateEmail(email, setValidationError) &&
+            validatePassword(password, setValidationError) &&
+            (isLogin ||
+                samePasswords(password, repeatPassword, setValidationError))
+        ) {
+            const auth = getAuth()
+
+            let authPromise
+            if (isLogin) {
+                authPromise = signInWithEmailAndPassword(
+                    auth,
+                    emailAfterTrim,
+                    password,
                 )
-                navigate(PROFILE_ROUTE)
-            })
-            .catch((error) => {
-                // setErrorState(error.code)
-                if (error.code) {
-                    console.log('errorCode: ', error.code)
-                    setErrorState(error.code)
-                    console.log('errorState: ', errorState)
-                }
-
-                switch (error.code) {
-                    case 'auth/user-not-found':
-                        setErrorState('Такого пользователя не существует!')
-                        break
-                    case 'auth/wrong-password':
-                        setErrorState('Неверный пароль!')
-                        break
-                    default:
-                        break
-                }
-            })
-    }
-
-    const handleRegister = async () => {
-        const auth = getAuth()
-        if (!email.trim()) {
-            return setErrorState('Введите email')
-        }
-        if (!password.trim()) {
-            return setErrorState('Введите пароль')
-        }
-        if (password.length < 6) {
-            return setErrorState('Пароль должен содержать не менее 6 символов')
-        }
-        if (password.trim() !== repeatPassword.trim()) {
-            setErrorState('Пароли не совпадают')
-        } else {
-            createUserWithEmailAndPassword(auth, email, password)
+            } else {
+                authPromise = createUserWithEmailAndPassword(
+                    auth,
+                    emailAfterTrim,
+                    password,
+                )
+            }
+            authPromise
                 .then(({ user }) => {
                     dispatch(
                         setAuth({
@@ -95,31 +64,15 @@ export function Auth() {
                             refreshToken: user.stsTokenManager.refreshToken,
                         }),
                     )
+                    setValidationError('')
                     navigate(PROFILE_ROUTE)
                 })
-                .catch((error) => {
-                    console.log(error.code)
-                    console.log(error.message)
-                    switch (error.code) {
-                        case 'auth/email-already-exists':
-                            setErrorState(
-                                'Адрес электронной почты уже используется существующим пользователем',
-                            )
-                            break
-                        case 'auth/invalid-email':
-                            setErrorState('Введен невалидный email')
-                            break
-                        default:
-                            break
-                    }
+                .catch((e) => {
+                    toast(e.code)
                 })
         }
     }
-    const isLogin = pathname === LOGIN_ROUTE
 
-    useEffect(() => {
-        setErrorState(null)
-    }, [email, password, repeatPassword])
     return (
         <div className={s.page}>
             <div className={s.wrapper}>
@@ -129,7 +82,10 @@ export function Auth() {
                     </div>
                     <Input
                         placeholder="Логин"
-                        classes={[s.login]}
+                        classes={[
+                            s.login,
+                            validationError === 'email' && s.error,
+                        ]}
                         type="text"
                         onChange={(e) => {
                             setEmail(e.target.value)
@@ -137,7 +93,10 @@ export function Auth() {
                     />
                     <Input
                         placeholder="Пароль"
-                        classes={[s.login]}
+                        classes={[
+                            s.login,
+                            validationError === 'password' && s.error,
+                        ]}
                         type="password"
                         onChange={(e) => {
                             setPassword(e.target.value)
@@ -146,29 +105,27 @@ export function Auth() {
                     {!isLogin && (
                         <Input
                             placeholder="Повторите пароль"
-                            classes={[s.login]}
+                            classes={[
+                                s.login,
+                                validationError === 'password' && s.error,
+                            ]}
                             type="password"
                             onChange={(e) => {
                                 setRepeatPassword(e.target.value)
                             }}
                         />
                     )}
-                    {errorState && (
-                        <div className={s.inputError}>{errorState} </div>
-                    )}
-                    <Link to={PROFILE_ROUTE}>
-                        <Button
-                            color={'purple'}
-                            onClick={isLogin ? handleLogin : handleRegister}
-                        >
-                            {isLogin ? 'Войти' : 'Зарегистрироваться'}
-                        </Button>
-                    </Link>
-                    <Link to={isLogin ? REGISTRATION_ROUTE : LOGIN_ROUTE}>
-                        <Button color={'light'}>
-                            {!isLogin ? 'Войти' : 'Зарегистрироваться'}
-                        </Button>
-                    </Link>
+                    <Button color={'purple'} onClick={auth}>
+                        {isLogin ? 'Войти' : 'Зарегистрироваться'}
+                    </Button>
+                    <Button
+                        color={'light'}
+                        onClick={() =>
+                            navigate(isLogin ? REGISTRATION_ROUTE : LOGIN_ROUTE)
+                        }
+                    >
+                        {!isLogin ? 'Войти' : 'Зарегистрироваться'}
+                    </Button>
                 </form>
             </div>
         </div>
