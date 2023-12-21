aa3ef32e38f9af1a61dc3970431930e5f718e1c7
DianaSemenova <menestrelia199702@gmail.com>
2023-12-21 19:23:25 +0400

    deleted userQuantityExercises store

--- src/store/slices/courses.js
+++ src/store/slices/courses.js
@@ -5,7 +5,6 @@ const initialState = {
     userCourses: [],
     workOut: {},
     workOutType: {},
-    userQuantityExercises: [],
 }
 
 const coursesSlice = createSlice({
@@ -28,19 +27,6 @@ const coursesSlice = createSlice({
         },
         setWorkOutType: (state, action) => {
             state.workOutType = action.payload
-        },
-        setUserQuantityExercises: (state) => {
-            const auth = JSON.parse(localStorage.getItem('auth'))
-            if (state.workOut) {
-                state.userQuantityExercises = state.workOut?.exercises?.map(
-                    (exercise) => {
-                        const user = exercise.users.find(
-                            (user) => user.userID === auth.userID,
-                        )
-                        return user?.quantityUser
-                    },
-                )
-            }
         },
     },
 })
