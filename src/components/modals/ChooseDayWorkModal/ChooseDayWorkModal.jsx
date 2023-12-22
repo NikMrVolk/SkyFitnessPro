603e84816406e9aaed343bcbed1c2f47c6a26c2a
Ulyana <ushinkevich@mail.ru>
2023-12-22 15:11:01 +0400

    prog: comparison of exercises

--- src/components/modals/ChooseDayWorkModal/ChooseDayWorkModal.jsx
+++ src/components/modals/ChooseDayWorkModal/ChooseDayWorkModal.jsx
@@ -1,12 +1,31 @@
 import { useNavigate } from 'react-router-dom'
 import s from './ChooseDayWorkModal.module.css'
 import { WORKOUT_ROUTE } from '../../../utils/constants'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { setWorkOut } from '../../../store/slices/courses'
+import { useState } from 'react'
 
 const ChooseDayWorkModal = ({ workOuts }) => {
     const dispatch = useDispatch()
     const navigate = useNavigate()
+
+    const [isDone, setIsDone] = useState(false)
+
+    const { workOut } = useSelector((state) => state.courses)
+    const { userID } = useSelector((state) => state.auth)
+    console.log('userID', userID)
+
+    const allProgress = workOut?.exercises?.map((el) => el.quantity)
+    console.log('allProgress', allProgress)
+
+    const userProgress = workOut?.exercises?.map((el) =>
+        el.users?.filter((user) => user.userID === userID),
+    )
+    console.log('userProgress', userProgress) //выводит не то
+
+    if (JSON.stringify(allProgress) == JSON.stringify(userProgress)) {
+        setIsDone(true)
+    }
 
     const handleClick = (el) => {
         dispatch(setWorkOut(el))
@@ -24,11 +43,9 @@ const ChooseDayWorkModal = ({ workOuts }) => {
                         key={el.day}
                         onClick={() => handleClick(el)}
                     >
-                        <div
-                            className={el.done ? s.modalBtnActive : s.modalBtn}
-                        >
+                        <div className={isDone ? s.modalBtnActive : s.modalBtn}>
                             <h3 className={s.exerciseTitle}>{el.name}</h3>
-                            {el.done && (
+                            {isDone && (
                                 <img
                                     className={s.modalImg}
                                     src="../img/isDone.svg"
